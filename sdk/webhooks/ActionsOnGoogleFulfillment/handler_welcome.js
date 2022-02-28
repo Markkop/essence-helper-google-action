dab8d003b15d72efa7609a70050ed7b8488a4485
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-02-28 14:52:39 -0300

    feat: add welcome suggestion

--- sdk/webhooks/ActionsOnGoogleFulfillment/handler_welcome.js
+++ sdk/webhooks/ActionsOnGoogleFulfillment/handler_welcome.js
@@ -1,5 +1,8 @@
 const { t } = require('i18next');
+const { Suggestion } = require('@assistant/conversation');
 
 exports.welcomeHandler = (conv) => {
   conv.add(t('LAUNCH'));
+  conv.add(new Suggestion({ title: 'Equipment Perk Effect' }));
+  conv.add(new Suggestion({ title: 'Cooking Ingredient Location' }));
 }
